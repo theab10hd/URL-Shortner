61396c0e4eb3af113ec68e99c24a2c26eee4cb2b
Abhijith Gaganan <theab10hd@gmail.com>
2025-02-18 23:31:04 +0530

    cors policy update

--- backend/index.js
+++ backend/index.js
@@ -10,13 +10,7 @@ const urlRoutes = require("./src/routes/url.routes");
 
 const app = express();
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-    methods: "GET, POST, PUT, DELETE",
-  })
-);
+app.use(cors());
 
 app.use(cookieParser());
 app.use(express.json());
