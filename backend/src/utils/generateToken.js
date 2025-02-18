ae98734cd7e5be88d8533e8322fd88f4e7c6f5e1
Abhijith Gaganan <theab10hd@gmail.com>
2025-02-18 23:53:17 +0530

    Update generateToken.js

--- backend/src/utils/generateToken.js
+++ backend/src/utils/generateToken.js
@@ -13,7 +13,7 @@ exports.generateToken = (userId, res) => {
   res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true,
-    sameSite: "Strict",
+    sameSite: "none",
     secure: process.env.NODE_ENV !== "development",
   });
 
